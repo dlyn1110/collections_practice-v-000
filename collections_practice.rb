282b9dc7804910205bdbef1ced28199e1f6627d1
Dani Lyn <dlyn1110@gmail.com>
2018-04-23 17:28:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by(&:length)
+end
+
+def swap_elements()
+end
+
+def reverse_array(arr)
+  arr.reverse!
+end
+
+def kesha_maker
+end
+
+def find_a(arr)
+arr.select { |word| word.start_with?('a') }
+end
+
+def sum_array
+end
+
+def add_s
+end
